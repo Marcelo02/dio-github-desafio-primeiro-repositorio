0ccd98f7e44f807f76155f0657c4188ce4b464d6
Marcelo02 <marcelo.manhaes.02@gmail.com>
2022-01-21 17:22:14 -0300

    Arquivo de programa de dominando IDEs criado

---
+++ Curso_Java/Dominando_IDEs/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+
+		/*
+		 * int a = 3; int b = 4; System.out.println("Quanto e " + a + " + " + b + " ?");
+		 * System.out.println("e igual a: " + (a+b));
+		 */
+
+	}
+}
