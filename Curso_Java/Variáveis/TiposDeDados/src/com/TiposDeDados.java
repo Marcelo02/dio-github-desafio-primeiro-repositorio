2d1c79d68cf439e8c97d2653e272e42129fcca3f
Marcelo02 <marcelo.manhaes.02@gmail.com>
2022-01-21 22:37:40 -0300

    Melhor rever aula de tipos de dados

---
+++ Curso_Java/Variáveis/TiposDeDados/src/com/TiposDeDados.java
@@ -0,0 +1,13 @@
+package com;
+
+public class TiposDeDados {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        int a = i++;
+
+        System.out.println(a);
+        System.out.println(i);
+    }
+}
