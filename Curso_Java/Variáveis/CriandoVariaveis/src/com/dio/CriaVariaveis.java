207b2de4d81a07fc6010c04fcde36a45df35ca1d
Marcelo02 <marcelo.manhaes.02@gmail.com>
2022-01-21 21:07:52 -0300

    Criando arquivo de criar variáveis

---
+++ Curso_Java/Variáveis/CriandoVariaveis/src/com/dio/CriaVariaveis.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+public class CriaVariaveis {
+
+    public static void main(String[] args) {
+
+        final int NUMERO_CONSTANTE = 15;
+        //Constantes, representadas por "final" usam letras maiúsculas.
+        int Idade = 18;
+        String playerName2 = "Jorge";
+        //Misturar letras e números não tem problema, e a notação camelo exige letra maiúscula nas palavras que não são a primeira.
+
+        System.out.println(NUMERO_CONSTANTE);
+        System.out.println(Idade);
+        System.out.println(playerName2);
+    }
+}
